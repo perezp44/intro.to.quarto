810a659f7bc012aa4ff3d36a93c090de48f71af9
Pedro J. Perez <pedro.j.perez@uv.es>
2023-03-08 16:59:44 +0100

    slides 05

--- slides/_slides_cortar.R
+++ slides/_slides_cortar.R
@@ -14,4 +14,25 @@
 
 title: "Mi primer blog con Quarto [(y Rmd)]{.fragment  .strike}"
 
-## Estructura del taller {.smaller}
+## Estructura del taller {.smaller}   Use the .smaller class to use a smaller typeface so that more text fits on the slide
+
+::: {.incremental}
+- Eat spaghetti
+- Drink wine
+:::
+  
+  ::: {.nonincremental}
+
+
+## Slide Title {.scrollable}
+
+Both of these options can also be applied globally to all slides as follows:
+---
+format:
+  revealjs:
+    smaller: true
+    scrollable: true
+---
+      
+      
+::: {.notes}
